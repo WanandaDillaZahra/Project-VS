f81181dd2c286d5998b86b7e9467df441822422b
WANANDA DILLA ZAHRA <110005462+WanandaDillaZahra@users.noreply.github.com>
2022-12-01 19:43:12 +0700

    Add files via upload

---
+++ Aplikasi Konversi Suhu/Aplikasi Konversi Suhu/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Aplikasi_Konversi_Suhu
+{
+    public partial class Form1 : Form
+    {
+        string a, b, c;
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            a = textBox1.Text;
+            b = comboBox1.Text;
+            c = comboBox2.Text;
+            g = Convert.ToDouble(a);
+            if ((b.Equals("celcius")) && (c.Equals("reamur")))
+            {
+                hitung = g / 5 * 4;
+                textBox2.Text = hitung.ToString();
+            }
+            else if ((b.Equals("celcius")) && (c.Equals("fahrenheit")))
+            {
+                hitung = g / 5 * 9 + 32;
+                textBox2.Text = hitung.ToString();
+            }
+            else if ((b.Equals("celsius")) && (c.Equals("kelvin")))
+            {
+                hitung = g / +273.15;
+                textBox2.Text = hitung.ToString();
+            }
+            else if ((b.Equals("reamur")) && (c.Equals("celcius")))
+        }
+
+        double g, hitung;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Bab 8 - Form Fakultas/Bab 8 - Form Fakultas/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Bab_8___Form_Fakultas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGTampil = new System.Windows.Forms.DataGridView();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnBatal = new System.Windows.Forms.Button();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtKode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGTampil)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGTampil
+            // 
+            this.DGTampil.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGTampil.Location = new System.Drawing.Point(177, 240);
+            this.DGTampil.Name = "DGTampil";
+            this.DGTampil.RowTemplate.Height = 28;
+            this.DGTampil.Size = new System.Drawing.Size(692, 305);
+            this.DGTampil.TabIndex = 17;
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnKeluar.Location = new System.Drawing.Point(757, 174);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(112, 35);
+            this.btnKeluar.TabIndex = 16;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = false;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnHapus.Location = new System.Drawing.Point(609, 174);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(112, 35);
+            this.btnHapus.TabIndex = 15;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnUpdate.Location = new System.Drawing.Point(463, 174);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(112, 35);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnBatal
+            // 
+            this.btnBatal.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnBatal.Location = new System.Drawing.Point(319, 174);
+            this.btnBatal.Name = "btnBatal";
+            this.btnBatal.Size = new System.Drawing.Size(112, 35);
+            this.btnBatal.TabIndex = 13;
+            this.btnBatal.Text = "Batal";
+            this.btnBatal.UseVisualStyleBackColor = false;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSimpan.Location = new System.Drawing.Point(177, 174);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(112, 35);
+            this.btnSimpan.TabIndex = 12;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = false;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(305, 118);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(564, 26);
+            this.txtNama.TabIndex = 10;
+            // 
+            // txtKode
+            // 
+            this.txtKode.Location = new System.Drawing.Point(305, 66);
+            this.txtKode.Name = "txtKode";
+            this.txtKode.Size = new System.Drawing.Size(399, 26);
+            this.txtKode.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nama Fakultas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Kode Fakultas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 546);
+            this.Controls.Add(this.DGTampil);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnBatal);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtKode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DGTampil)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGTampil;
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnBatal;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtKode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
