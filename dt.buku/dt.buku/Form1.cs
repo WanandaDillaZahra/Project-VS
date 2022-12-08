15719190691c1637c847ebda098c13cb74ba11eb
WANANDA DILLA ZAHRA <110005462+WanandaDillaZahra@users.noreply.github.com>
2022-12-08 19:12:09 +0700

    Add files via upload

---
+++ dt.buku/dt.buku/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace dt.buku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            koneksidatabase koneksi = new koneksidatabase();
+            DataTable dt = new DataTable();
+            dt = koneksi.tampil();
+            dataGridView1.DataSource = dt;
+        }
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            koneksidatabase kont = new koneksidatabase();
+            kont.QUERY("INSERT INTO tb_buku VALUES('" + this.dateTimePicker1.Text + "','" + this.textBox1.Text + "','" + this.textBox2.Text + "','" + this.textBox3.Text +"')");
+            MessageBox.Show("insert data berhasil");
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            koneksidatabase kont = new koneksidatabase();
+            kont.QUERY("UPDATE tb_buku set tahun_terbit = '" + this.dateTimePicker1.Text + "', penulis = '" + this.textBox2.Text + "',penerbit='" + this.textBox3.Text + "' WHERE judul_buku='" + this.textBox1.Text + "'");
+            MessageBox.Show("Update success");
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            koneksidatabase kont = new koneksidatabase();
+            kont.QUERY("DELETE * from tb_buku WHERE judul_buku = '" + textBox1.Text + "'");
+           
+            MessageBox.Show("Delete success");
+        }
+    }
+}
---
+++ dt.buku/dt.buku/koneksidatabase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data;
+
+namespace dt.buku
+{
+    class koneksidatabase
+    {
+        string database = "provider=Microsoft.ACE.OLEDB.12.0;Data Source= databuku.accdb";
+        string sql = " SELECT * FROM tb_buku";
+        public OleDbConnection koneksi;
+        public OleDbCommand perintah;
+        public OleDbDataAdapter adp;
+        public void connect()
+        {
+            koneksi = new OleDbConnection(database);
+            koneksi.Open();
+
+        }
+        public void disconected()
+        {
+            koneksi = new OleDbConnection(database);
+            koneksi.Close();
+        }
+
+        public DataTable tampil()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                connect();
+                perintah = new OleDbCommand(sql, koneksi);
+                adp = new OleDbDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                adp.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return dt;
+        }
+        public void QUERY(string query)
+        {
+            try
+            {
+                connect();
+                perintah = new OleDbCommand(query, koneksi);
+                perintah.ExecuteNonQuery();
+            }
+            catch (Exception ali)
+            {
+                MessageBox.Show(ali.Message);
+            }
+            finally
+            {
+                disconected();
+            }
+        }
+    }
+}
